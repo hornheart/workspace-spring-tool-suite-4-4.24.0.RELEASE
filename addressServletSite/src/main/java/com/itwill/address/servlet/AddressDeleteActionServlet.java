a7aaba7db05a2599a2bc488644b3450ef31fc083
hornheart <hornheart@naver.com>
2024-09-12 18:11:45 +0900

    09121811

--- addressServletSite/src/main/java/com/itwill/address/servlet/AddressDeleteActionServlet.java
+++ addressServletSite/src/main/java/com/itwill/address/servlet/AddressDeleteActionServlet.java
@@ -1,7 +1,9 @@
 	package com.itwill.address.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
+import com.itwill.address.Address;
 import com.itwill.address.AddressService;
 
 import jakarta.servlet.ServletException;
@@ -17,15 +19,17 @@ import jakarta.servlet.http.HttpServletResponse;
 public class AddressDeleteActionServlet extends HttpServlet {
 	
 
-	
+	@Override
 	protected void service(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8");
+		PrintWriter out = response.getWriter();
+//		String noStr=request.getParameter("no");
 		/*
 		 요청라인  GET /addressSite/address_delete_action?no=1 HTTP/1.1
 		 요청헤더  ....
 		 요청바디 없다
 		 */
-
 		/*
 		 * 0.요청객체encoding설정
 		 * 1.파라메타받기
@@ -35,10 +39,19 @@ public class AddressDeleteActionServlet extends HttpServlet {
 		*/ 
 		
 		try {
-			
+			request.setCharacterEncoding("UTF-8");
+			String noStr = request.getParameter("no");
+//			String name=request.getParameter("name");
+//			String phone = request.getParameter("phone");
+//			String addr= request.getParameter("address");
+//			Address address=new Address(0, name, phone, addr);
+
+			AddressService addressService=new AddressService();
+			int deleteRowCount=addressService.addressDelete(Integer.parseInt(noStr));
+			response.sendRedirect("address_list");
 		} catch (Exception e) {
 			e.printStackTrace();
-
+			response.sendRedirect("address_error.html");
 		}
 	}
 
