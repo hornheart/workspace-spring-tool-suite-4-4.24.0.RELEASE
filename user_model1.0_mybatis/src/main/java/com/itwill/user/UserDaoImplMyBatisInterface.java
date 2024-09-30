a91beaf7d5dd34898479c28f3b4ec251929b5fee
hornheart <hornheart@naver.com>
2024-09-30 20:38:20 +0900

    09302038

--- user_model1.0_mybatis/src/main/java/com/itwill/user/UserDaoImplMyBatisInterface.java
+++ user_model1.0_mybatis/src/main/java/com/itwill/user/UserDaoImplMyBatisInterface.java
@@ -3,8 +3,12 @@ package com.itwill.user;
 import java.util.List;
 
 import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import com.itwill.user.mapper.UserMapper;
+
 
 public class UserDaoImplMyBatisInterface implements UserDao {
 
@@ -13,49 +17,64 @@ public class UserDaoImplMyBatisInterface implements UserDao {
 	public UserDaoImplMyBatisInterface() throws Exception {
 		this.sqlSessionFactory = new SqlSessionFactoryBuilder()
 				.build(Resources.getResourceAsStream("mybatis-config.xml"));
+		SqlSession sqlSession=sqlSessionFactory.openSession(true);
+		
+		sqlSession.close();
 	}
 
 	@Override
 	public int update(User user) throws Exception {
-		System.out.println("#### UserDaoImplMyBatis : update() 호출  ");
-		int rowCount=0;
+		SqlSession sqlSession=sqlSessionFactory.openSession(true);
+		System.out.println("#### UserDaoImplMyBatis : update() 호출  "+sqlSession);
+		int rowCount=sqlSession.getMapper(UserMapper.class).update(user);
+		sqlSession.close();
 		return rowCount;
 	}
 
 	@Override
 	public User findUser(String userId) throws Exception {
+		SqlSession sqlSession=sqlSessionFactory.openSession(true);
 		System.out.println("#### UserDaoImplMyBatis : findUser() 호출  ");
-		
-		User user=null;
+		User user=(User) sqlSession.getMapper(UserMapper.class).findUser(userId);
+		sqlSession.close();
 		return user;
 	}
 
 	@Override
 	public List<User> findUserList() throws Exception {
-		System.out.println("#### UserDaoImplMyBatis : findUserList 호출  ");
-		List<User> userList=null;
+		SqlSession sqlSession=sqlSessionFactory.openSession(true);
+		System.out.println("#### UserDaoImplMyBatis : findUserList 호출  "+sqlSession);
+		UserMapper userMapper=sqlSession.getMapper(UserMapper.class);
+		List userList=userMapper.findUserList();
+//		List<User> userList=sqlSession.getMapper(UserMapper.class).findUserList();
+		sqlSession.close();
 		return userList;
 	}
 
 	@Override
 	public int insert(User user) throws Exception {
-		System.out.println("#### UserDaoImplMyBatis : insert() 호출  ");
-		int rowCount=0;
+		SqlSession sqlSession=sqlSessionFactory.openSession(true);
+		System.out.println("#### UserDaoImplMyBatis : insert() 호출  "+sqlSession);
+		int rowCount=sqlSession.getMapper(UserMapper.class).insert(user);
+		sqlSession.close();
 		return rowCount;
 	}
 
 	@Override
 	public int delete(String userId) throws Exception {
-		System.out.println("#### UserDaoImplMyBatis : delete() 호출  ");
-		
-		int rowCount=0;
+		SqlSession sqlSession=sqlSessionFactory.openSession(true);
+		System.out.println("#### UserDaoImplMyBatis : delete() 호출  "+sqlSession);
+		int rowCount=sqlSession.getMapper(UserMapper.class).delete(userId);
+		sqlSession.close();
 		return rowCount;
 	}
 
 	@Override
 	public int countByUserId(String userId) throws Exception {
-		System.out.println("#### UserDaoImplMyBatis : countByUserId 호출  ");
-		int rowCount=0;
+		SqlSession sqlSession=sqlSessionFactory.openSession(true);
+		System.out.println("#### UserDaoImplMyBatis : countByUserId 호출  "+sqlSession);
+		int rowCount=sqlSession.getMapper(UserMapper.class).countByUserId(userId);
+		sqlSession.close();
 		return rowCount;
 	}
 
--- user_model1.0_mybatis/src/main/java/com/itwill/user/mapper/UserMapper.java
+++ user_model1.0_mybatis/src/main/java/com/itwill/user/mapper/UserMapper.java
@@ -1,5 +1,41 @@
 package com.itwill.user.mapper;
 
+import java.util.List;
+
+
+import org.apache.ibatis.annotations.ResultType;
+import org.apache.ibatis.annotations.Select;
+
+import com.itwill.user.User;
+
 public interface UserMapper {
 
+	@ResultType(com.itwill.user.User.class)
+	@Select("select userid,password,name,email from userinfo")
+	public List<User> findUserList(); 
+	
+	@ResultType(com.itwill.user.User.class)
+	@Select("select userid,password,name,email from userinfo where userid=#{userId}")
+	public User findUser(String userId);
+	
+	@ResultType(com.itwill.user.User.class)
+	@Select("delete userinfo where userid=#{userId}")
+	public int delete(String userId);
+	
+	@ResultType(com.itwill.user.User.class)
+	@Select("update  userinfo set password=#{password},name=#{name},email=#{email} "
+			+ "where userid=#{userId}")
+	public int update(User user);
+	
+	@ResultType(com.itwill.user.User.class)
+	@Select("insert into userinfo(userid,password,name,email) "
+			+ "values(#{userId},"
+			+ "#{password},"
+			+ "#{name},"
+			+ "#{email})")
+	public int insert(User user);
+	
+	@ResultType(com.itwill.user.User.class)
+	@Select("select count(*) cnt from userinfo where userid=#{userId}")
+	public int countByUserId(String userId);
 }
