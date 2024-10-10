bc64f4da808ee95b8e1fd01237243418cc55c656
hornheart <hornheart@naver.com>
2024-10-10 12:23:20 +0900

    10101223

---
+++ testboard/src/main/java/com/testboard/TestboardApplication.java
@@ -0,0 +1,13 @@
+package com.testboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestboardApplication.class, args);
+	}
+
+}
---
+++ testboard/src/test/java/com/testboard/TestboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
