d39c716d9b9f99f503fea0eacc32f875af0cd062
hornheart <hornheart@naver.com>
2024-11-13 17:43:49 +0900

    11131743 jpa_web_application1

---
+++ src/main/java/com/itwill/jpa/SpringJpaWebApplication1.java
@@ -0,0 +1,14 @@
+package com.itwill.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaWebApplication1  {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaWebApplication1.class, args);
+	}
+	
+
+}
