9552e7f206445e31884f66b7f3ed22923aab7351
hornheart <hornheart@naver.com>
2024-11-28 18:56:06 +0900

    11281856

---
+++ spring_web_application-2/src/main/java/com/itwill/SpringWebApplication2Application.java
@@ -0,0 +1,13 @@
+package com.itwill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebApplication2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebApplication2Application.class, args);
+	}
+
+}
---
+++ spring_web_application-2/src/test/java/com/itwill/SpringWebApplication2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itwill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebApplication2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
